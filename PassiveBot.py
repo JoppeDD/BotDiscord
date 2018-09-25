1ff50f4dfdf6556a8c3e43fe11026b85eeaaf0ae
joppe <joppe.duthoit@gmail.com>
2018-09-25 17:41:16 +0200

    laptop naar pc overzet

---
+++ PassiveBot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+Token = 'NDkzODY0MTY3Mjg0MDE1MTA0.DovY1A.we3DSt029qKH2DL9uzSVeMYH9rk'
+
+client = commands.Bot(command_prefix = '?')
+
+@client.event
+async def on_ready():
+    print('ready')
+
+
+@client.event
+async def on_message(message):
+    author = message.author
+    message = message.content
+    print('{} sent by {}'.format(message, author))
+
+
+# @client.event
+# async def on_message_delete(message):
+#     author = message.author
+#     content = message.content
+#     channel = message.channel
+#     await client.send_message(channel, '{}: {}'.format(content, author))
+
+
+
+
+client.run(Token)
